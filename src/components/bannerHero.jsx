b393c0ddab667c040f4fd78e78a5c8908e3cd920
V. Puentes <68936403+V-Puentes@users.noreply.github.com>
2025-08-28 18:29:24 -0400

    actualización paleta de colores bannerHero.jsx

--- src/components/bannerHero.jsx
+++ src/components/bannerHero.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 const BannerHero = ({ title, description, imageUrl, imageAlt }) => {
   return (
-    <div className="banner-hero bg-primary text-white " style={{ backgroundColor: '#3a1a56' }}>
+    <div 
+      className="banner-hero bg-primary text-white" 
+      style={{ backgroundColor: 'var(--rosado-oscuro)' }}  
+    >
       <div className="container">
         <div className="row align-items-center">
           <div className="col-md-6">
@@ -12,7 +15,7 @@ const BannerHero = ({ title, description, imageUrl, imageAlt }) => {
           <div className="col-md-6 text-center">
             <img 
               src='/assets/png/logoRocket2.png'
-              alt='portada'
+              alt={imageAlt || 'portada'}
               className="img-fluid rounded shadow-lg"
               height="300"
               width="300"  
